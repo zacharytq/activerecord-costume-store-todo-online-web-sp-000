1d2bdf9dd447d5ef5d85e521220dfbb7a6c5ff5c
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:28:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -30,10 +30,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string "name"
   end
 
 end
