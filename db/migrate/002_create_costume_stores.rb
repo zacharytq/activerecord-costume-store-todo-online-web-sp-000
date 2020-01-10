6cbeff6d9d11da84bd438764862c57584406dcd6
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:25:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,12 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
+      t.boolean :still_in_business
       t.integer :costume_inventory
       t.integer :num_of_employees
+      t.datetime :opening_time
+      t.datetime :closing_time
+      
     end
   end
 end
