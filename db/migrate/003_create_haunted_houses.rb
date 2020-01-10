32ed4ba5c8d343361cde83180d910645642b976c
chic-page-6502 <zacharytq@gmail.com>
2020-01-10 01:28:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,10 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
       t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
     end
   end
 end
